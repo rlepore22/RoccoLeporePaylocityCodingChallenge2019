5abf1eb1978d8135e783d21c70586afb23512f46
rlepore22 <36007923+rlepore22@users.noreply.github.com>
2019-09-29 21:11:35 -0500

    Add project files.

---
+++ RoccoLeporePaylocityCodingChallenge2019/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace RoccoLeporePaylocityCodingChallenge2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmployee = new System.Windows.Forms.Label();
+            this.txtEmployee = new System.Windows.Forms.TextBox();
+            this.lblSpouse = new System.Windows.Forms.Label();
+            this.txtSpouse = new System.Windows.Forms.TextBox();
+            this.lblOtherDependents = new System.Windows.Forms.Label();
+            this.txtOther = new System.Windows.Forms.TextBox();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.lblAns = new System.Windows.Forms.Label();
+            this.lblDirections = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEmployee
+            // 
+            this.lblEmployee.AutoSize = true;
+            this.lblEmployee.Location = new System.Drawing.Point(12, 21);
+            this.lblEmployee.Name = "lblEmployee";
+            this.lblEmployee.Size = new System.Drawing.Size(90, 13);
+            this.lblEmployee.TabIndex = 0;
+            this.lblEmployee.Text = "Employee Name: ";
+            // 
+            // txtEmployee
+            // 
+            this.txtEmployee.Location = new System.Drawing.Point(108, 18);
+            this.txtEmployee.Name = "txtEmployee";
+            this.txtEmployee.Size = new System.Drawing.Size(100, 20);
+            this.txtEmployee.TabIndex = 1;
+            // 
+            // lblSpouse
+            // 
+            this.lblSpouse.AutoSize = true;
+            this.lblSpouse.Location = new System.Drawing.Point(12, 65);
+            this.lblSpouse.Name = "lblSpouse";
+            this.lblSpouse.Size = new System.Drawing.Size(77, 13);
+            this.lblSpouse.TabIndex = 2;
+            this.lblSpouse.Text = "Spouse Name:";
+            // 
+            // txtSpouse
+            // 
+            this.txtSpouse.Location = new System.Drawing.Point(108, 62);
+            this.txtSpouse.Name = "txtSpouse";
+            this.txtSpouse.Size = new System.Drawing.Size(100, 20);
+            this.txtSpouse.TabIndex = 3;
+            // 
+            // lblOtherDependents
+            // 
+            this.lblOtherDependents.AutoSize = true;
+            this.lblOtherDependents.Location = new System.Drawing.Point(12, 123);
+            this.lblOtherDependents.Name = "lblOtherDependents";
+            this.lblOtherDependents.Size = new System.Drawing.Size(94, 13);
+            this.lblOtherDependents.TabIndex = 4;
+            this.lblOtherDependents.Text = "Other Dependents";
+            // 
+            // txtOther
+            // 
+            this.txtOther.Location = new System.Drawing.Point(12, 180);
+            this.txtOther.Multiline = true;
+            this.txtOther.Name = "txtOther";
+            this.txtOther.Size = new System.Drawing.Size(149, 101);
+            this.txtOther.TabIndex = 5;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(10, 304);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(92, 30);
+            this.btnPreview.TabIndex = 6;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // lblAns
+            // 
+            this.lblAns.AutoSize = true;
+            this.lblAns.Location = new System.Drawing.Point(167, 183);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(0, 13);
+            this.lblAns.TabIndex = 7;
+            // 
+            // lblDirections
+            // 
+            this.lblDirections.AutoSize = true;
+            this.lblDirections.Location = new System.Drawing.Point(12, 149);
+            this.lblDirections.Name = "lblDirections";
+            this.lblDirections.Size = new System.Drawing.Size(228, 13);
+            this.lblDirections.TabIndex = 8;
+            this.lblDirections.Text = "Please have the names seperated by a comma";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 366);
+            this.Controls.Add(this.lblDirections);
+            this.Controls.Add(this.lblAns);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.txtOther);
+            this.Controls.Add(this.lblOtherDependents);
+            this.Controls.Add(this.txtSpouse);
+            this.Controls.Add(this.lblSpouse);
+            this.Controls.Add(this.txtEmployee);
+            this.Controls.Add(this.lblEmployee);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmployee;
+        private System.Windows.Forms.TextBox txtEmployee;
+        private System.Windows.Forms.Label lblSpouse;
+        private System.Windows.Forms.TextBox txtSpouse;
+        private System.Windows.Forms.Label lblOtherDependents;
+        private System.Windows.Forms.TextBox txtOther;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Label lblAns;
+        private System.Windows.Forms.Label lblDirections;
+    }
+}
+
---
+++ RoccoLeporePaylocityCodingChallenge2019/Form1.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * Rocco Lepore
+ * 9/30/2019
+ * Paylocity Coding Challenge
+ */
+using System;
+using System.Windows.Forms;
+
+namespace RoccoLeporePaylocityCodingChallenge2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            double totalCost = 0;//total benefit cost
+            double paycheck = 2000;//total of each pay check
+            double numPay = 26;//number of paychecks the employee recieves each year
+            double benefitCost = 1000;//amount of money the benefit cost for the entire year
+            double numDependents;//number of dependents that the employee has
+            double dependentCost = 500;//cost incured for each dependent
+            double totalDependentCost;//total cost of all the dependents
+            double discountAmount;//discount
+            double costPerPay;//amount taken out of every paycheck
+            double finalCheck;
+
+            //checks to see if a name was entered into the employee textbox. If not the calculations do not run.
+            if (String.IsNullOrWhiteSpace(txtEmployee.Text))
+            {
+                MessageBox.Show("Please enter an employee before clicking the preview button.");
+                return;
+            }
+            //doesn't let program run if numbers are entered into the name fields
+            int outParse;
+            if (int.TryParse(txtEmployee.Text, out outParse) || int.TryParse(txtSpouse.Text, out outParse) || int.TryParse(txtOther.Text, out outParse))
+            {
+                MessageBox.Show("Please do not enter any numbers into any of the text boxes");
+                return;
+            }
+
+            numDependents = calcNumDependents();
+            discountAmount = calcDiscount();
+
+            totalDependentCost = numDependents * dependentCost;
+            totalCost = ((benefitCost + totalDependentCost) * (1 - discountAmount));
+            costPerPay = (totalCost / numPay);
+            finalCheck = paycheck - costPerPay;
+
+            //rounds values after calculations have already been made
+            totalCost = Math.Round(totalCost, 2);
+            costPerPay = Math.Round(costPerPay, 2);
+            finalCheck = Math.Round(finalCheck, 2);
+
+            lblAns.Text = "Number of Dependents: " + numDependents + "\n"
+                + "Benefit Cost: $" + totalCost + "\n"
+                + "Total Discount: " + (discountAmount * 100) + "%\n"
+                + "Deduction from Paycheck: $" + costPerPay + "\n"
+                + "Paycheck Total: $" + finalCheck;
+        }
+
+        //method that calulates the number of dependents
+        public double calcNumDependents()
+        {
+            double ans = 0;
+            //looks to see if textbox is empty. If not than add 1 to the total 
+            if (!(String.IsNullOrEmpty(txtSpouse.Text)))
+            {
+                ans = ans + 1;
+            }
+            // looks to see if textbox is empty. If not splits the lines up into an array and then counts them
+            if (!(String.IsNullOrEmpty(txtOther.Text)))
+            {
+                string[] lines = splitOtherTxt();
+                int count = lines.Length;
+                ans = ans + count;
+            }
+            return ans;
+        }
+
+        //calculates the total discount given based on all the names entered
+        public double calcDiscount()
+        {
+            double ans = 0;
+            //checks if the  Employee name starts with the letter a. If so adds discount
+            if (txtEmployee.Text.Trim().StartsWith("A") || txtEmployee.Text.Trim().StartsWith("a"))
+            {
+                ans = ans + .1;
+            }
+            //checks if the  Spouse name starts with the letter 'A'. If so adds discount
+            if (txtSpouse.Text.Trim().StartsWith("A") || txtSpouse.Text.Trim().StartsWith("a"))
+            {
+                ans = ans + .1;
+            }
+            //splits up the "Other Dependents" text box 
+            if (!(String.IsNullOrEmpty(txtOther.Text)))
+            {
+                string[] lines = splitOtherTxt();
+                //checks to see if each line starts with the letter 'A'. If so adds to discount
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].ToString().Trim().StartsWith("A") || lines[i].ToString().Trim().StartsWith("a"))
+                    {
+                        ans = ans + .1;
+                    }
+                }
+            }
+            return ans;
+        }
+
+        public string[] splitOtherTxt()
+        {
+            return txtOther.Text.Split(new Char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
